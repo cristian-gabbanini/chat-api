b45b9e4175ff9b5251367083932e407f1354f61f
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-04-11 18:14:39 +0200

    Refactoring della funzione chat

--- src/chat.ts
+++ src/chat.ts
@@ -10,8 +10,7 @@ interface Connection {
 }
 
 type MessageEvent = { ts: number; type: "message"; content: ChatMessage };
-type UserOnlineEvent = { ts: number; type: "user-online"; user: User };
-type UserOfflineEvent = { ts: number; type: "user-offline"; user: User };
+
 type UserEnterRoomEvent = {
   ts: number;
   type: "enter-room";
@@ -26,12 +25,7 @@ type UserLeaveRoomEvent = {
   room: Room;
 };
 
-export type ChatEvent =
-  | MessageEvent
-  | UserOnlineEvent
-  | UserOfflineEvent
-  | UserEnterRoomEvent
-  | UserLeaveRoomEvent;
+export type ChatEvent = MessageEvent | UserEnterRoomEvent | UserLeaveRoomEvent;
 
 export interface ChatDriver {
   (user: User): {
@@ -59,22 +53,6 @@ export type ChatMessage = {
   user: User;
   room: Room;
 };
-
-function onMessage(
-  this: ReturnType<ChatDriver>,
-  conn: Connection,
-  fn: (message: ChatMessage) => void
-): void {
-  this.listen(event => {
-    if (event.type === "message") {
-      if (conn.roomId === event.content.room.id) {
-        if (event.content.user.id !== this.user.id) {
-          fn(event.content);
-        }
-      }
-    }
-  });
-}
 
 function onEnterRoom(
   this: ReturnType<ChatDriver>,
@@ -104,30 +82,6 @@ function disconnect(this: ReturnType<ChatDriver>) {
   this.disconnect();
 }
 
-function enterRoom(
-  this: ReturnType<ChatDriver>,
-  conn: Connection,
-  roomId: string
-) {
-  const enter: UserEnterRoomEvent = {
-    ts: Date.now(),
-    type: "enter-room",
-    room: { id: roomId },
-    user: this.user
-  };
-
-  this.trigger(enter);
-  conn.roomId = roomId;
-
-  return () =>
-    this.trigger({
-      ts: Date.now(),
-      type: "leave-room",
-      room: { id: roomId },
-      user: this.user
-    });
-}
-
 function sendMessage(this: ReturnType<ChatDriver>, message: ChatMessage) {
   this.trigger({
     ts: Date.now(),
@@ -139,15 +93,53 @@ function sendMessage(this: ReturnType<ChatDriver>, message: ChatMessage) {
 
 export function chat(driver: ChatDriver, user: User): Connection {
   const boundDriver = driver(user);
-  const conn: any = {
+
+  let currentRoomId: string | null;
+
+  const conn: Connection = {
+    enterRoom: enterRoom.bind(boundDriver),
     sendMessage: sendMessage.bind(boundDriver),
     disconnect: disconnect.bind(boundDriver),
+    onMessage: onMessage.bind(boundDriver),
     onEnterRoom: onEnterRoom.bind(boundDriver),
     onLeaveRoom: onLeaveRoom.bind(boundDriver),
     driver: boundDriver
   };
-  conn.enterRoom = enterRoom.bind(boundDriver, conn);
-  conn.onMessage = onMessage.bind(boundDriver, conn);
 
-  return conn as Connection;
+  function enterRoom(this: ReturnType<ChatDriver>, roomId: string) {
+    const enter: UserEnterRoomEvent = {
+      ts: Date.now(),
+      type: "enter-room",
+      room: { id: roomId },
+      user: this.user
+    };
+
+    this.trigger(enter);
+    currentRoomId = roomId;
+
+    return () =>
+      this.trigger({
+        ts: Date.now(),
+        type: "leave-room",
+        room: { id: roomId },
+        user: this.user
+      });
+  }
+
+  function onMessage(
+    this: ReturnType<ChatDriver>,
+    fn: (message: ChatMessage) => void
+  ): void {
+    this.listen(event => {
+      if (event.type === "message") {
+        if (currentRoomId === event.content.room.id) {
+          if (event.content.user.id !== this.user.id) {
+            fn(event.content);
+          }
+        }
+      }
+    });
+  }
+
+  return conn;
 }
