d05dc6a029f04dde45c9accb854d76e9676afa6b
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-04-16 17:51:08 +0200

    Aggiunge test onopen

--- src/__mocks__/WebSocket.ts
+++ src/__mocks__/WebSocket.ts
@@ -1,12 +1,23 @@
 export function __WebSocket(url: string, protocol: string) {
+  const onOpen: Function[] = [];
   const socket = {
     close: () => {},
     send: (data: any) => {},
-    onopen: (event: Event) => {},
+    onopen: (fn: (event: Event) => void): void => {
+      onOpen.push(fn);
+    },
     onmessage: (event: Event) => {},
     onerror: (event: Event) => {},
     protocol,
   };
+
+  setTimeout(() => {
+    const e: Event = {
+      type: 'ONOPEN',
+      state: 'READY',
+    };
+    onOpen.forEach(listener => listener(e));
+  }, 15);
 
   return socket;
 }
--- src/__tests__/websocket.test.ts
+++ src/__tests__/websocket.test.ts
@@ -6,7 +6,15 @@ declare global {
   }
 }
 
-window.WebSocket = __WebSocket;
+beforeEach(() => {
+  window.WebSocket = __WebSocket;
+});
+
+function wait(ms: number) {
+  return new Promise(res => {
+    setTimeout(() => res(true), ms);
+  });
+}
 
 test('Creates a mock WebSocket', () => {
   const socket = new WebSocket('http://chat.somehost.org', 'json');
@@ -16,4 +24,12 @@ test('Creates a mock WebSocket', () => {
   expect(socket).toHaveProperty('onmessage');
   expect(socket).toHaveProperty('onerror');
   expect(socket.protocol).toBe('json');
+});
+
+test('Triggers the onopen event', async () => {
+  const socket = new WebSocket('http://chat.somehost.org', 'json');
+  const onOpenHandler = jest.fn();
+  socket.onopen(onOpenHandler);
+  await wait(20);
+  expect(onOpenHandler).toHaveBeenCalledTimes(1);
 });
